5988d7894fbbeb6a29f77e83ebb083e696c02419
Nicolas Mauricio Martins Coiado <48792614+NicolasCoiado@users.noreply.github.com>
2022-12-10 08:08:18 -0300

    Add arquivos

---
+++ src/JavaFoundations2.java
@@ -0,0 +1,33 @@
+public class JavaFoundations2 {
+    public static void main (String [] args){
+        System.out.println(" "+" "+"■"+"■"+"■"+"■"+" "+" ");
+        System.out.println(" "+"■"+" "+" "+" "+" "+"■"+" ");
+        System.out.println("■"+" "+"■"+" "+" "+"■"+" "+"■");
+        System.out.println("■"+" "+" "+" "+" "+" "+" "+"■");
+        System.out.println("■"+" "+"■"+" "+" "+"■"+" "+"■");
+        System.out.println("■"+" "+" "+"■"+"■"+" "+" "+"■");
+        System.out.println(" "+"■"+" "+" "+" "+" "+"■"+" ");
+        System.out.println(" "+" "+"■"+"■"+"■"+"■"+" " +" ");
+        System.out.println(" ");
+        System.out.println ("    /:''|    ");
+        System.out.println ("   |: 66|_   ");
+        System.out.println ("   C     _)  ");
+        System.out.println ("    \\ ._|      ");
+        System.out.println ("     ) /       ");
+        System.out.println ("   /`\\       ");
+        System.out.println ("   || |Y|       ");
+        System.out.println ("   || |#|       ");
+        System.out.println ("   || |#|       ");
+        System.out.println ("   || |#|       ");
+        System.out.println ("   :| |=:       ");
+        System.out.println ("   ||_|,|      ");
+        System.out.println ("   \\)))||     ");
+        System.out.println ("|~~~`-`~~~|   ");
+        System.out.println ("|         |    ");
+        System.out.println ("|_________|    ");
+        System.out.println ("|_________|    ");
+        System.out.println ("    | ||       ");
+        System.out.println ("    |_||__        ");
+        System.out.println ("    (____))    ");
+    }
+}
